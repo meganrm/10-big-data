de9ecc4878018b66d7a1087bacc03094ed3da9a7
meganrm <meganrm@gmail.com>
2016-08-29 10:57:23 -0700

    finsihed out top five method

--- scripts/zip.js
+++ scripts/zip.js
@@ -2,30 +2,63 @@
   function Zip(opts){
     this.zip = opts.properties.zip;
     this.neighborhood = opts.properties.neighborhood;
-    this.address = opts.properties.address;
-    this.coordinates = opts.geometry.coordinates
+    if (opts.properties.address){
+      this.address = opts.properties.address;
+    }
+    else {
+      this.address = null;
+    }
+    this.coordinates = {
+      lat :     opts.geometry.coordinates[0],
+      lng :     opts.geometry.coordinates[1]
+    };
   }
 
-    Zips = []
-
-  // Transform the initial collection into
-  // a new array of objects with just the properties we specify below.
-  //
-  // - The zip code
-  // - Its neighborhood
-  // - Its address (if applicable, otherwise null)
-  // - Ensure that this new collection also contains the coordinates for each zip. The "coordinates" property should be its own object with "lat" and "lng" properties since we'd like to incorporate Google Maps later.
+  Zip.allZips = [];
 
   getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
       // TODO: start here!
-      console.log(data);
+      // console.log(data);
       data.features.forEach(function(ele){
-        Zips.push(new Zip(ele));
+        Zip.allZips.push(new Zip(ele));
       });
+      console.log(Zip.topZips().splice(0,5));
+
     });
   };
 
+  Zip.neighborhoodZips = function(){
+    return Zip.allZips.reduce(function(acc, cur, index, array){
+      if (!acc.find(function(ele){
+        return ele.neighborhood === cur.neighborhood;
+      })) {
+        obj = {};
+        obj.neighborhood = cur.neighborhood;
+        obj.zips = 1;
+        acc.push(obj);
+      }
+      else {
+        // console.log('already in array');
+        currentIndex = acc.findIndex(function(ele){
+          return ele.neighborhood === cur.neighborhood;
+        }
+        );
+        acc[currentIndex].zips = acc[currentIndex].zips + 1;
+      }
+      return acc;
+    }, []);
+  };
+
+  Zip.topZips = function(){
+    return Zip.neighborhoodZips().sort(function(a, b){
+      return b.zips - a.zips;
+    });
+  };
+
+
+
   getData();
   module.Zip = Zip;
+
 })(window);
